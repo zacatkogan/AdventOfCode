750f9b1f45913d829a60a318e29acd7293326e07
Zac Faragher <1051830+elvishfiend@users.noreply.github.com>
2023-12-05 08:46:26 +1100

    2018 Day 1 and 2

---
+++ AoC2018/Day01.cs
@@ -0,0 +1,36 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2018
+{
+    internal class Day01 : BaseDay
+    {
+        public override object Solve1()
+        {
+            return DataLines.Select(x => int.Parse(x)).Sum();
+        }
+
+        public override object Solve2()
+        {
+            var ints = DataLines.Select(int.Parse).ToList();
+            var freqs = new HashSet<int>();
+            var f = 0;
+            freqs.Add(0);
+
+            while (true)
+            {
+                foreach (var i in ints)
+                {
+                    f += i;
+
+                    if (!freqs.Add(f))
+                        return f;
+                }
+            }
+        }
+    }
+}
---
+++ AoC2018/Day02.cs
@@ -0,0 +1,48 @@
+﻿using AdventOfCode;
+using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2018
+{
+    internal class Day02 : BaseDay
+    {
+        public override object Solve1()
+        {
+            var c2 = 0;
+            var c3 = 0;
+
+            foreach (var l in DataLines)
+            {
+                var g = l.GroupBy(x => x);
+                if (g.Any(x => x.Count() == 2))
+                    c2 += 1;
+
+                if (g.Any(x => x.Count() == 3))
+                    c3 += 1;
+            }
+
+            return c2 * c3;
+        }
+
+        public override object Solve2()
+        {
+            var joined = from a in DataLines
+            from b in DataLines
+            where a != b
+                && Enumerable.Zip(a, b).Count(x => x.First != x.Second) == 1
+            select (a, b);
+
+            return joined.Select(
+                x => string.Join(
+                    "", 
+                    x.a.Zip(x.b)
+                        .Where(c => c.First == c.Second)
+                        .Select(c => c.First)))
+                .First();
+        }
+    }
+}
