437aeb66e2128df6c390575933120b19c43eabf4
Zac Faragher <1051830+elvishfiend@users.noreply.github.com>
2025-12-07 23:16:12 +1100

    Day 6 + 7

---
+++ AoC2025/Day_06.cs
@@ -0,0 +1,94 @@
+namespace AdventOfCode.AoC2025;
+
+public class Day_06 : BaseDay
+{
+    public override object Solve1()
+    {
+        var data = DataLines.Select(x => x.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+        var total = 0L;
+
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            var a = long.Parse(data[0][i]);
+            var b = long.Parse(data[1][i]);
+            var c = long.Parse(data[2][i]);
+            var d = long.Parse(data[3][i]);
+
+            var operation = data[4][i];
+            if (operation == "*")
+            {
+                total += (a * b * c * d);
+            }
+            else if (operation == "+")
+            {
+                total += (a + b + c + d);
+            }
+        }
+
+        return total;
+    }
+
+    public override object Solve2()
+    {
+        var data = DataLines;
+
+        var total = 0L;
+
+        var isNewOperation = true;
+        char operation = '+';
+        long opTotal = 0;
+        for (int i = 0; i < data[0].Length; i++)
+        {
+            var a = data[0][i].ToString();
+            var b = data[1][i].ToString();
+            var c = data[2][i].ToString();
+            var d = data[3][i].ToString();
+
+            var l = new List<string> { a, b, c, d };
+
+            if (l.All(string.IsNullOrWhiteSpace))
+            {
+                isNewOperation = true;
+                total += opTotal;
+                opTotal = 0;
+                continue;
+            }
+
+            if (isNewOperation)
+            {
+                isNewOperation = false;
+                operation = data[4][i];
+
+                if (operation == '*')
+                {
+                    opTotal = 1;
+                }
+            }
+
+            var num = 0L;
+            var str = a + b + c + d;
+            try
+            {
+                num = long.Parse(str);
+            }
+            catch
+            {
+            }
+
+            if (operation == '*')
+            {
+                opTotal *= num;
+            }
+            else if (operation == '+')
+            {
+                opTotal += num;
+            }
+
+        }
+
+        total += opTotal;
+
+        return total;
+    }
+}
---
+++ AoC2025/Day_07.cs
@@ -0,0 +1,80 @@
+using System.Collections.Concurrent;
+
+namespace AdventOfCode.AoC2025;
+
+public class Day_07 : BaseDay
+{
+    public override object Solve1()
+    {
+        var startIndex = DataLines[0].IndexOf('S');
+
+        var beams = new HashSet<Position>();
+        beams.Add((startIndex, 0));
+
+        int splits = 0;
+
+        for (int row = 1; row < DataLines.Length; row++)
+        {
+            var beamsPrevRow = beams.Where(beam => beam.Y == row - 1).ToList();
+            var currentRow = DataLines[row];
+
+            foreach (var beam in beamsPrevRow)
+            {
+                var x = beam.X;
+                if (currentRow[x] == '^')
+                {
+                    splits++;
+                    beams.Add((x - 1, row));
+                    beams.Add((x + 1, row));
+                }
+                else
+                {
+                    beams.Add((x, row));
+                }
+            }
+        }
+
+        return splits;
+    }
+
+    public override object Solve2()
+    {
+        // this is basically pascal's triangle with splitters
+        // count the number of ways to reach each position, this feeds down into later positions
+
+        var data = DataLines.Select(line => line.ToCharArray()).ToArray();
+        var startIndex = DataLines[0].IndexOf('S');
+
+        // use ConcurrentDictionary for AddOrUpdate method, easier than checking for existence first
+        var beams = new ConcurrentDictionary<Position, long>();
+
+        beams.TryAdd((startIndex, 0), 1);
+
+        for (int row = 1; row < data.Length; row++)
+        {
+            var currentRow = data[row];
+            
+            // project beams down from previous row
+            var projectedBeams = beams.Where(b => b.Key.Y == row - 1).ToList();
+
+            foreach (var (pos, ways) in projectedBeams)
+            {
+                var x = pos.X;
+                if (currentRow[x] == '^')
+                {
+                    // splitter
+                    beams.AddOrUpdate((x - 1, row), ways, (_, oldWays) => oldWays + ways);
+                    beams.AddOrUpdate((x + 1, row), ways, (_, oldWays) => oldWays + ways);
+                }
+                else
+                {
+                    // straight beam
+                    beams.AddOrUpdate((x, row), ways, (_, oldWays) => oldWays + ways);
+                }
+            }
+        }
+
+        var total = beams.Where(b => b.Key.Y == data.Length - 1).Sum(y => y.Value);
+        return total;
+    }
+}
